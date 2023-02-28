c1277de9d2f18b8dd144a0463b955ccd1ce1e7fa
Ransika Gunasekara <ransika1998@gmail.com>
2023-02-28 18:30:34 +0530

    finetune.py

--- finetune.py
+++ finetune.py
@@ -104,7 +104,7 @@ def evaluate_images(model, path, validloader, ep):
         plt.subplot(7, 12, (4 * i + 4));
         plt.axis('off');
         plt.title('Side', fontsize=6)
-        plt.imshow(data[i] * np.tile(pred[i, 2, :, :, np.newaxis], (1, 1, 3)))
+        # plt.imshow(data[i] * np.tile(pred[i, 2, :, :, np.newaxis], (1, 1, 3)))
     image_name = os.path.join(path, '%i.png' % (ep + 1))
     plt.savefig(image_name, dpi=200);
     plt.close()
